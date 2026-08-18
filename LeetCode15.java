e5b6dc5faad2244074c6509f4506f1f0bef51476
Abrar-Abhirama <13523038@std.stei.itb.ac.id>
2026-08-18 15:49:14 +0700

    solved: 15 O(n2)

--- LeetCode15.java
+++ LeetCode15.java
@@ -25,9 +25,53 @@ class Sum3Solution{
     }
 }
 
+class Sum3OptimalSolution{
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>() {};
+        Arrays.sort(nums);
+        int left, right, total;
+        for (int i = 0; i < nums.length; i++){
+            if (i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+
+            left = i + 1;
+            right = nums.length-1;
+
+            while (left < right){
+                total = nums[i] + nums[left] + nums[right];
+                if (total == 0){
+                    result.add(List.of(nums[i], nums[left], nums[right]));
+                    
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left -1]){
+                        left++;
+                    }
+                    while (left< right && nums[right] == nums[right + 1]){
+                        right --;
+                    }
+
+                }
+
+                else if (total  < 0){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+        }
+
+
+        return result;
+    }
+}
+
 public class LeetCode15 {
     public static void main(String[] args){
-        Sum3Solution solution = new Sum3Solution();
+        Sum3OptimalSolution solution = new Sum3OptimalSolution();
         int [] nums = {-1,0,1,2,-1,-4};
         List<List<Integer>> result = solution.threeSum(nums);
         System.out.println(result);
