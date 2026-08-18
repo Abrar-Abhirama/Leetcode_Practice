76fb5f621118dfecfd58156c81963ca03a6d2e9f
Abrar-Abhirama <13523038@std.stei.itb.ac.id>
2026-08-19 00:54:00 +0700

    solved: 11 O(n)

--- LeetCode11.java
+++ LeetCode11.java
@@ -1,3 +1,4 @@
+import java.security.KeyStore.Entry;
 import java.util.*;
 
 class ContainerSolution{
@@ -17,11 +18,34 @@ class ContainerSolution{
     }
 }
 
+class ContainerOptimalSolution{
+    public int maxArea(int[] height){
+        int result = 0;
+        int left = 0;
+        int right = height.length -1;
+
+        while (left < right){
+            int lowest = Math.min(height[left], height[right]);
+            int total = (right-left) * lowest;
+
+            result = Math.max(total, result);
+            if (height[left] < height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+
+        return result;
+    }
+}
+
 
 public class LeetCode11 {
     public static void main(String[] args){
-        ContainerSolution solution = new ContainerSolution();
-        int[] height = {1,1};
+        ContainerOptimalSolution solution = new ContainerOptimalSolution();
+        int[] height = {1,8,6,2,5,4,8,3,7};
         int result = solution.maxArea(height);
         System.out.println(result);
     }
