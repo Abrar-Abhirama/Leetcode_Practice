475369ec3cbb88ae3f6443f40bab27b32d1da289
Abrar-Abhirama <13523038@std.stei.itb.ac.id>
2026-08-20 10:07:43 +0700

    solved : 121

--- LeetCode121.java
+++ LeetCode121.java
@@ -1,3 +1,22 @@
-public class LeetCode121 {
-    
+class StockSolution{
+    public int maxProfit(int[] prices) {
+        int result = 0;
+        int cheapest = prices[0];
+        for (int i = 0; i < prices.length ; i++){
+            cheapest = Math.min(cheapest, prices[i]);
+            result = Math.max((prices[i] - cheapest), result);
+        }
+
+        return result;
+    }
 }
+
+public class LeetCode121 {
+    public static void main(String[] args){
+        StockSolution solution = new StockSolution();
+        int[] prices = {7,1,5,3,6,4};
+        int result = solution.maxProfit(prices);
+        System.out.println(result);
+    }
+}
+
