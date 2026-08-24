217c753ebfd2bf4f46b6b0bfca7a5e145e952ded
Abrar-Abhirama <13523038@std.stei.itb.ac.id>
2026-08-24 09:57:25 +0700

    solved : 424

--- LeetCode424.java
+++ LeetCode424.java
@@ -1,3 +1,42 @@
-public class LeetCode424 {
-    
+import java.util.*;
+
+class LongestRepeatSolution{
+    public int characterReplacement(String s, int k) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        s = s.toUpperCase();
+        int left = 0, result = 0, maxFreq = 0;
+        for (int right = 0; right < s.length(); right++){
+            char current = s.charAt(right);
+
+            map.put(current, map.getOrDefault(current, 0) + 1);
+            maxFreq = Math.max(maxFreq, map.get(current));
+
+            int windowSize = right - left + 1;
+            int replace = windowSize - maxFreq;
+
+            if (replace > k){
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar) - 1);
+                left++;
+            }
+
+            result = Math.max(result, right - left + 1);
+            
+        }
+
+        return result;
+    }
 }
+
+public class LeetCode424 {
+    public static void main(String[] args){
+        LongestRepeatSolution solution = new LongestRepeatSolution();
+        String s = "aababba";
+        int k = 2;
+        int result = solution.characterReplacement(s, k);
+        System.out.println(result);
+    }
+}
+
+// aaba b
+
