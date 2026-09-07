51e1ddbbb85873b5d332aa23296a6ab7e4c76ff2
Abrar-Abhirama <13523038@std.stei.itb.ac.id>
2026-09-07 08:42:45 +0700

    solved : 138

---
+++ LeetCode138.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
+class DeepCopySolution {
+    public Node copyRandomList(Node head) {
+        HashMap<Node, Node> map = new HashMap<>();
+        Node curr = head;
+        while (curr != null){
+            map.put(curr, new Node (curr.val));
+            curr = curr.next;
+        }
+
+        curr = head;
+        Node copy;
+
+        while (curr != null){
+            copy = map.get(curr);
+            copy.next = map.get(curr.next);
+            copy.random = map.get(curr.random);
+            curr = curr.next;
+        }
+        return map.get(head);
+    }
+}
+
+public class LeetCode138 {
+    
+}
