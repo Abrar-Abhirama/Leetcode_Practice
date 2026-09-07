15d1770ea4cf7a482b2d9c7385faacee5635254b
Abrar-Abhirama <13523038@std.stei.itb.ac.id>
2026-09-07 11:06:01 +0700

    solved: 74 optimal

--- LeetCode74.java
+++ LeetCode74.java
@@ -34,6 +34,48 @@ class Search2DSolution {
     }
 }
 
+class OptimalSearch2DSolution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int top = 0;
+        int bottom = matrix.length - 1;
+
+        // Find the correct row
+        while (top <= bottom) {
+            int row = top + (bottom - top) / 2;
+
+            if (target >= matrix[row][0] &&
+                target <= matrix[row][matrix[row].length - 1]) {
+
+                // Binary search inside the row
+                int left = 0;
+                int right = matrix[row].length - 1;
+
+                while (left <= right) {
+                    int mid = left + (right - left) / 2;
+
+                    if (matrix[row][mid] == target) {
+                        return true;
+                    } else if (matrix[row][mid] < target) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+
+                return false;
+            }
+
+            if (target < matrix[row][0]) {
+                bottom = row - 1;
+            } else {
+                top = row + 1;
+            }
+        }
+
+        return false;
+    }
+}
+
 public class LeetCode74 {
     public static void main(String[] args){
         int[][] matrix = {{1,3,5,7}, {10,11,16,20}, {23,30,34,60}};
