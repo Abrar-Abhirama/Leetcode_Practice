66db6234b90e0148628128fdad150e7e05a9aba6
Abrar-Abhirama <13523038@std.stei.itb.ac.id>
2026-09-18 17:02:41 +0700

    solved: 100

--- LeetCode104.java
+++ LeetCode104.java
@@ -16,7 +16,7 @@ class MaximumDepthSolution {
         if (root == null){
             return 0;
         }
-
+        
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
         return 1 + Math.max(left, right);
